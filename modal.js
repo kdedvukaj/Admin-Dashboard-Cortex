09d88b7c93148a7aa81c6cb6f70231e92c3cbf02
kdedvukaj <kdedvukaj94@gmail.com>
2022-08-16 22:28:34 +0200

    Added the modal style dropdown

--- modal.js
+++ modal.js
@@ -2,6 +2,17 @@ let modal = document.querySelector("#modal");
 let overlay = document.querySelector("#overlay");
 let resetPsw = document.querySelector("#reset-psw");
 let closeModalBtn = document.querySelector("#close-modal");
+let bxEditBtns = document.querySelectorAll("#bx-edit-book");
+console.log(bxEditBtns);
+
+bxEditBtns.forEach((item) => {
+  item.addEventListener("click", () => {
+    modal.style.display = "block";
+    overlay.style.display = "block";
+  });
+});
+
+overlay.addEventListener("click", closeModal);
 
 resetPsw.addEventListener("click", () => {
   modal.style.display = "block";
