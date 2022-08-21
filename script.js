ba86c34fc7a697e552a654f5d44a9506dacbeb7d
kdedvukaj <kdedvukaj94@gmail.com>
2022-08-21 11:53:12 +0200

    changed styles, added search func
    changed the input, search,select styles, added the search func

--- script.js
+++ script.js
@@ -52,6 +52,28 @@ window.addEventListener("click", (e) => {
   }
 });
 
+function tableSearch() {
+  // Declare variables
+  let input, filter, table, tr, td, i, txtValue;
+  input = document.getElementById("searchInput");
+  filter = input.value.toUpperCase();
+  table = document.getElementById("searchTable");
+  tr = table.getElementsByTagName("tr");
+
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td")[1];
+    if (td) {
+      txtValue = td.textContent || td.innerText;
+      if (txtValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
 // // Remove Profile Dropdown by clicking outside btn for Mobile
 // window.addEventListener("touchend", (e) => {
 //   if (e.target !== dropLink) {
