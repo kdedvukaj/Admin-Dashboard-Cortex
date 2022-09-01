e4aaf454cb494926cedff919f2780eef84ef72a5
kdedvukaj <kdedvukaj94@gmail.com>
2022-09-01 21:02:01 +0200

    Added logo link, show more btn, clr classes, red stars and more...

--- admin/script.js
+++ admin/script.js
@@ -127,6 +127,34 @@ function deleteRow(r) {
   }
 }
 
+window.onload = function () {
+  var chart = new CanvasJS.Chart("chartContainer", {
+    animationEnabled: true,
+    theme: "light2", // "light1", "light2", "dark1", "dark2"
+    title: {
+      text: "Trenutno Stanje Knjiga",
+    },
+    axisY: {
+      suffix: "%",
+    },
+
+    data: [
+      {
+        type: "column",
+        yValueFormatString: '#,##0.0#"%"',
+        dataPoints: [
+          { label: "Izdate", y: 71 },
+          { label: "Vracene", y: 67 },
+          { label: "Prekoracenju", y: 50 },
+          { label: "Aktivne", y: 56 },
+          { label: "Arhivirane", y: 30 },
+        ],
+      },
+    ],
+  });
+  chart.render();
+};
+
 // // Remove Profile Dropdown by clicking outside btn for Mobile
 // window.addEventListener("touchend", (e) => {
 //   if (e.target !== dropLink) {
