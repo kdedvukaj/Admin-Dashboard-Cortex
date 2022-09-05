9efcdac9aa30de26e644c7a6eeecdc3d378094a5
kdedvukaj <kdedvukaj94@gmail.com>
2022-09-05 21:28:16 +0200

    Changed some js scripts for canvas, textarea and date

--- admin/script.js
+++ admin/script.js
@@ -9,6 +9,37 @@ let dropLink = document.querySelector(".drop-link");
 let profile = document.querySelector(".profile");
 let imgBtn = document.querySelector(".img-btn");
 
+let mainDate = document.querySelector("#main-date");
+let secondDate = document.querySelector("#second-date");
+
+window.onload = function () {
+  var chart = new CanvasJS.Chart("chartContainer", {
+    animationEnabled: true,
+    theme: "light2", // "light1", "light2", "dark1", "dark2"
+    title: {
+      text: "Trenutno Stanje Knjiga",
+    },
+    axisY: {
+      suffix: "%",
+    },
+
+    data: [
+      {
+        type: "column",
+        yValueFormatString: '#,##0.0#"%"',
+        dataPoints: [
+          { label: "Izdate", y: 71 },
+          { label: "Vracene", y: 67 },
+          { label: "Prekoracenju", y: 50 },
+          { label: "Aktivne", y: 56 },
+          { label: "Arhivirane", y: 30 },
+        ],
+      },
+    ],
+  });
+  chart.render();
+};
+
 function toggleCheck(source) {
   let tableHead = document.querySelector("#table-head");
   let deleteTable = document.querySelector("#delete-table");
@@ -155,6 +186,13 @@ window.onload = function () {
   chart.render();
 };
 
+mainDate.addEventListener("input", () => {
+  console.log(mainDate.value);
+  let fixedDate = new Date(mainDate.value);
+  fixedDate.setDate(fixedDate.getDate() + 30);
+  secondDate.value = fixedDate.toISOString("yyyy-MM-dd").split("T")[0];
+});
+
 // // Remove Profile Dropdown by clicking outside btn for Mobile
 // window.addEventListener("touchend", (e) => {
 //   if (e.target !== dropLink) {
@@ -177,7 +215,3 @@ window.onload = function () {
 //   modal.classList.remove("open");
 //   overlay.classList.remove("open");
 // }
-
-ClassicEditor.create(document.querySelector("#editor")).catch((error) => {
-  console.error(error);
-});
