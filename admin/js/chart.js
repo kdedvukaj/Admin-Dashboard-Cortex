d6723e8465423ec6ad22a95628bce439f38013c8
kdedvukaj <kdedvukaj94@gmail.com>
2022-09-10 11:39:35 +0200

    ChartJs added

--- admin/js/chart.js
+++ admin/js/chart.js
@@ -1,29 +1,39 @@
-// CanvaJs Chart Script
+// ChartJs Chart Script
 
-window.onload = function () {
-  var chart = new CanvasJS.Chart("chartContainer", {
-    animationEnabled: true,
-    theme: "light2", // "light1", "light2", "dark1", "dark2"
-    title: {
-      text: "Trenutno Stanje Knjiga",
-    },
-    axisY: {
-      suffix: "%",
-    },
-
-    data: [
+const ctx = document.getElementById("myChart").getContext("2d");
+const myChart = new Chart(ctx, {
+  type: "bar",
+  data: {
+    labels: ["Izdate", "Vracene", "Prekoracenju", "Aktivne", "Arhivirane"],
+    datasets: [
       {
-        type: "column",
-        yValueFormatString: '#,##0.0#"%"',
-        dataPoints: [
-          { label: "Izdate", y: 71 },
-          { label: "Vracene", y: 67 },
-          { label: "Prekoracenju", y: 50 },
-          { label: "Aktivne", y: 56 },
-          { label: "Arhivirane", y: 30 },
+        label: "Trenutno Stanje Knjiga",
+        data: [61, 57, 40, 46, 20],
+        backgroundColor: [
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
         ],
+        borderColor: [
+          "rgba(153, 102, 255, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 99, 132, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(255, 206, 86, 1)",
+        ],
+        borderWidth: 1,
       },
     ],
-  });
-  chart.render();
-};
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+    responsive: true,
+    maintainAspectRatio: false,
+  },
+});
